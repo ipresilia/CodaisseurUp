19f29ef52a7ca9ccd8c020ae73be04f62d191019
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-29 14:13:34 +0200

    Created Photo: Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
 
   def show
     @themes = @event.themes
+    @photos = @event.photos
   end
 
   def new
@@ -18,18 +19,32 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to @event, notice: "Event successfully created"
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path(@event),
+      notice: "Event successfully created"
     else
       render :new
     end
   end
 
   def edit
+    if current_user.id == @event.user.id
+      @photos = @event.photos
+    else
+      redirect_to root_path, notice: "You do not have persmission."
+    end
   end
 
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: "Event successfully updated"
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path, notice: "Event successfully updated"
     else
       render :edit
     end
@@ -42,5 +57,9 @@ class EventsController < ApplicationController
 
     def event_params
       params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, :user, theme_ids: [])
+    end
+
+    def image_params
+      params[:images].present? ? params.require(:images) : []
     end
 end
